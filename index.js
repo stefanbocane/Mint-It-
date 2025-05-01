08e7bd750f0063a868de42cd61ef8c84e827f166
stefanbocane <stefanbocane@gmail.com>
2025-05-01 13:30:29 -0700

    fix: dual register main & auth, remove stray registrations

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { registerRootComponent } from 'expo';
+import { AppRegistry } from 'react-native';
 import 'react-native-gesture-handler';
 import App from './App';
 
-registerRootComponent(App); 
+// 👉 Expo normally registers "main".  We ALSO register "auth" to satisfy any library that expects it.
+registerRootComponent(App);               // registers "main"
+AppRegistry.registerComponent('auth', () => App); 
